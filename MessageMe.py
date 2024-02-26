6bca971cad10a87a787c3f1f085e9576c7f31c49
keeganob <71083040+keeganob@users.noreply.github.com>
2024-02-26 16:50:49 +0200

    Add files via upload

---
+++ MessageMe.py
@@ -0,0 +1,1 @@
+print("MessageMe PeerToPeer Application")
---
+++ server.py
@@ -0,0 +1,24 @@
+from socket import *
+serverName = 'localhost'
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print('The server is ready to recieve')
+while True: 
+	try:
+		connectionSocket, addr = serverSocket.accept()
+		sentence = connectionSocket.recv(1024).decode()
+		#sentence recieves the data from the client into the server
+	
+		print(f"Connection from {addr} has been established")
+		print(f"Recieved info: {sentence}") 
+		capSentence = sentence.upper()
+		connectionSocket.send(capSentence.encode())
+		#Sends Message to client 
+
+		connectionSocket.close() 
+		#close the connection 
+	except Exception as e:
+		print(f"An Error occured: {e}")
+
